a79715587ec7e1ee3da860144c0c4d311a5262cb
nogards95TG <page271295@gmail.com>
2025-11-14 13:18:47 +0100

    refactor: migrate to flat structure
    - Remove monorepo structure (packages/core, client, server)
    - Move all source code to root src/ directory
    - Consolidate into single publishable package
    - Update all import paths to use relative paths
    - Remove workspace dependencies and turbo
    - Simplify build configuration
    - Build successfully generates ESM + CJS + types

--- packages/client/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './rpc/RpcClient';
-export * from './pubsub/Publisher';
-
-// Testing utilities
-export { MockRpcClient } from '../__tests__/mocks/MockRpcClient';
-export { MockPublisher } from '../__tests__/mocks/MockPublisher';
